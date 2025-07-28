afa45b7e5135c9341fd7d150f7e5070458f4faca
Dani-error <zdani.error@gmail.com>
2025-07-28 13:31:48 +0100

    fix: forgot import

--- runtime.js
+++ runtime.js
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+import { fileURLToPath } from 'url'
 
 let globalsCache = new Map();
 let stackDeobfuscator = null;
